1196216994e09a0c9c879ef19ecfc514ea2b7a1f
DontCode <zoo_sistem@hotmail.com>
2016-02-06 19:01:19 -0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(".days")
+   .countdown("2016/02/27", function(event) {
+     $(this).text(
+       event.strftime('%D')
+    );
+});
+
+$(".hours")
+   .countdown("2016/01/27", function(event) {
+     $(this).text(
+       event.strftime('%H')
+    );
+});
+
+$(".minutes")
+   .countdown("2016/01/27", function(event) {
+     $(this).text(
+       event.strftime('%M')
+    );
+});
+
+$(".seconds")
+   .countdown("2016/01/27", function(event) {
+     $(this).text(
+       event.strftime('%S')
+    );
+});   
