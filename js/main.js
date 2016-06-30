f734bfab6b0cfc6ed1546139372be1c9a77f9cae
Einsquare <einsqr@gmail.com>
2016-06-29 23:06:10 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 $(".days")
-   .countdown("2016/02/27", function(event) {
+   .countdown("2016/07/20", function(event) {
      $(this).text(
        event.strftime('%D')
     );
 });
 
 $(".hours")
-   .countdown("2016/02/27", function(event) {
+   .countdown("2016/07/20", function(event) {
      $(this).text(
        event.strftime('%H')
     );
 });
 
 $(".minutes")
-   .countdown("2016/02/27", function(event) {
+   .countdown("2016/07/20", function(event) {
      $(this).text(
        event.strftime('%M')
     );
 });
 
 $(".seconds")
-   .countdown("2016/02/27", function(event) {
+   .countdown("2016/07/20", function(event) {
      $(this).text(
        event.strftime('%S')
     );
